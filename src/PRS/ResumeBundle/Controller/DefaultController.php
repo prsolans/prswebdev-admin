29dddb231baeedc893aa73531f05dc610b84a368
prsolans <paul@aqdct.com>
2014-07-26 16:50:42 -0500

    Created / default route for post login

--- src/PRS/ResumeBundle/Controller/DefaultController.php
+++ src/PRS/ResumeBundle/Controller/DefaultController.php
@@ -8,12 +8,13 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
+
     /**
-     * @Route("/hello/{name}")
+     * @Route("/")
      * @Template()
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+		return $this->render('PRSResumeBundle:Default:index.html.twig');
     }
 }
